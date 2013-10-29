4ae4df73ef5e54136dc438e419d7d738f0d27e32
madsmpedersen <m@madsp.dk>
2013-10-29 08:59:00 +0100

    unittest of hawc2ascii2bin

---
+++ hawc2ascii2bin/unit_test/Test.py
@@ -0,0 +1,54 @@
+'''
+Created on 29/10/2013
+
+@author: mmpe
+'''
+
+from hawc2ascii2bin import hawc2ascii2bin
+from hawc2ascii2bin.hawc2ascii2bin import ascii2bin, size_from_file
+from hawc2ascii2bin.unittest import Hawc2io
+import numpy as np
+import os
+import sys
+import unittest
+from textui.textui import DaemonUI
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        sys.path.append("../")
+
+    def testAscii2bin(self):
+
+        for f in ["Hawc2ascii_bin.sel", "Hawc2ascii_bin.dat"]:
+            if os.path.exists(f):
+                os.remove(f)
+        ascii2bin("Hawc2ascii.sel")
+
+        ascii_file = Hawc2io.ReadHawc2('Hawc2ascii')
+        bin_file = Hawc2io.ReadHawc2("Hawc2ascii_bin")
+
+        np.allclose(ascii_file.ReadAscii(), bin_file.ReadBinary())
+        self.assertEqual(ascii_file.ChInfo, bin_file.ChInfo)
+
+    def testAscii2bin_new_name(self):
+        for f in ["Hawc2bin.sel", "Hawc2bin.dat"]:
+            if os.path.exists(f):
+                os.remove(f)
+        ascii2bin("Hawc2ascii.sel", "Hawc2bin.sel", DaemonUI())
+
+        ascii_file = Hawc2io.ReadHawc2('Hawc2ascii')
+        bin_file = Hawc2io.ReadHawc2("Hawc2bin")
+
+        np.allclose(ascii_file.ReadAscii(), bin_file.ReadBinary())
+        self.assertEqual(ascii_file.ChInfo, bin_file.ChInfo)
+
+
+    def testSizeOfFile(self):
+        self.assertEqual(size_from_file("hawc2ascii.sel"), (800, 28))
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
