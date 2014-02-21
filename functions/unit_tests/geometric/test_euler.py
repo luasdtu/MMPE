2d3231dfbaf2011b8a6bef75e735526006142e05
madsmpedersen <m@madsp.dk>
2014-02-21 12:44:58 +0100

    euler errors fixed

--- functions/unit_tests/geometric/test_euler.py
+++ functions/unit_tests/geometric/test_euler.py
@@ -9,55 +9,89 @@ from functions.geometric import rad, deg
 from functions.geometric.euler import *
 import numpy as np
 
-
+npEqual = np.testing.assert_array_equal
+npAlmostEqual = np.testing.assert_array_almost_equal
 class Test(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.npEqual = np.testing.assert_array_equal
-        self.npAlmostEqual = np.testing.assert_array_almost_equal
 
-    def test_xyz2euler(self):
-        np.testing.assert_array_almost_equal(xyz2euler(0, 0, 0), np.array([1, 0, 0, 0]))
-        e0 = np.cos(rad(45) / 2)
-        np.testing.assert_array_almost_equal(xyz2euler(rad(45), 0, 0), np.array([e0, 0, 0, np.sqrt(1 - e0 ** 2)]))
+
+
 
     def test_euler2angle(self):
-        self.assertAlmostEqual(euler2angle(xyz2euler(rad(45), 0, 0)), rad(45))
+        e0 = np.cos(rad(30) / 2)
+        self.assertAlmostEqual(euler2angle(np.array([e0, np.sqrt(1 - e0 ** 2), 0, 0])), rad(30))
 
 
     def test_euler2A(self):
-        x, y, z = rad(30) , 0, 0
-        e = xyz2euler(rad(30) , 0, 0)
-        s = np.sin(rad(30))
-        c = np.cos(rad(30))
-        self.npAlmostEqual(euler2A(e), np.array([[c, -s, 0],
-                                                 [s, c, 0],
-                                                 [0, 0, 1]]))
-
-    def test_A2xyz(self):
-        s = np.sin(rad(30))
-        c = np.cos(rad(30))
-        self.npEqual(np.array([0, 0, 30]), deg(A2xyz(np.array([[c, -s, 0],
-                                                               [s, c, 0],
-                                                               [0, 0, 1]]))))
+        e0 = np.cos(rad(30) / 2)
+        e1 = np.sqrt(1 - e0 ** 2)
+        npAlmostEqual(euler2A(np.array([e0, e1, 0, 0])), Ax(rad(30)))
+        npAlmostEqual(euler2A(np.array([e0, 0, e1, 0])), Ay(rad(30)))
+        npAlmostEqual(euler2A(np.array([e0, 0, 0, e1])), Az(rad(30)))
 
     def test_A2euler(self):
-        e = xyz2euler(rad(30), 0, 0)
-        A = euler2A(e)
-        self.npEqual(e, A2euler(A))
+        e0 = np.cos(rad(30) / 2)
+        e1 = np.sqrt(1 - e0 ** 2)
+        npAlmostEqual(A2euler(Ax(rad(30))), np.array([e0, e1, 0, 0]))
+        npAlmostEqual(A2euler(Ay(rad(30))), np.array([e0, 0, e1, 0]))
+        npAlmostEqual(A2euler(Az(rad(30))), np.array([e0, 0, 0, e1]))
 
 
-    def test_euler2gl(self):
-        self.npAlmostEqual(euler2gl(xyz2euler(rad(30), 0, 0)), np.array([ 30., 0. , 0.   , 0.25881905]))
+#    def test_xyz2A(self):
+#        print deg(A2xyz(xyz2A(rad(10), rad(12.5), rad(14.5))))
 
-    def test(self):
-        print xyz2euler(0, rad(60), 0)
-        e = xyz2euler(0, rad(60), 0)
-        A = euler2A(e)
-        print A2euler(A)
 
-        print np.round(euler2A([ 0.8660254 , 0.   , -0.5      , 0.       ]), 4)
 
+    #    def test_A2euler(self):
+#        e = xyz2euler(rad(30), 0, 0)
+#        A = euler2A(e)
+#        self.npEqual(e, A2euler(A))
+
+
+#
+#    def test_A2xyz(self):
+#        s = np.sin(rad(30))
+#        c = np.cos(rad(30))
+#        self.npEqual(np.array([30, 0, 0]), deg(A2xyz(np.array([[1, 0, 0],
+#                                                               [0, c, -s],
+#                                                               [0, s, c]]))))
+#        self.npEqual(np.array([0, 30, 0]), deg(A2xyz(np.array([[c, 0, s],
+#                                                               [0, 1, 0],
+#                                                               [-s, 0, c]]))))
+#        self.npEqual(np.array([0, 0, 30]), deg(A2xyz(np.array([[c, -s, 0],
+#                                                               [s, c, 0],
+#                                                               [0, 0, 1]]))))
+#
+#        A = np.array([[1, 0, 0], [0, c, -s], [0, s, c]])
+#        #self.npEqual(A, xyz2A(*A2xyz(A)))
+
+#
+#    def test_xyz2euler(self):
+#        np.testing.assert_array_almost_equal(xyz2euler(0, 0, 0), np.array([1, 0, 0, 0]))
+#        e0 = np.cos(rad(30) / 2)
+#        np.testing.assert_array_almost_equal(xyz2euler(rad(30), 0, 0), np.array([e0, np.sqrt(1 - e0 ** 2), 0, 0]))
+#        np.testing.assert_array_almost_equal(xyz2euler(0, rad(30), 0), np.array([e0, 0, np.sqrt(1 - e0 ** 2), 0]))
+#        np.testing.assert_array_almost_equal(xyz2euler(0, 0, rad(30)), np.array([e0, 0, 0, np.sqrt(1 - e0 ** 2)]))
+
+
+#    def test_A2euler(self):
+#        e = xyz2euler(rad(30), 0, 0)
+#        A = euler2A(e)
+#        self.npEqual(e, A2euler(A))
+#
+#
+#    def test_euler2gl(self):
+#        self.npAlmostEqual(euler2gl(xyz2euler(rad(30), 0, 0)), np.array([ 30., 0. , 0.   , 0.25881905]))
+#
+##    def test(self):
+##        print xyz2euler(0, rad(60), 0)
+##        e = xyz2euler(0, rad(60), 0)
+##        A = euler2A(e)
+##        print A2euler(A)
+##
+##        print np.round(euler2A([ 0.8660254 , 0.   , -0.5      , 0.       ]), 4)
+#
 
 
 if __name__ == "__main__":
